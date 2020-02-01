02567e3b961ff3ba4c342194cae53a9ad2920775
LaVi <lastvirgo266@gmail.com>
2020-02-01 23:50:52 +0900

    Init API Server

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
-const flash = require('connect-flash');
+const morgan = require('morgan');
 const session = require('express-session');
+const flash = require('connect-flash');
 require('dotenv').config();
-
-var router = require('./routes/router');
-
-
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');
-const userRouter = require('./routes/user');
 
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes');
+const v1 = require('./routes/v1');
+const v2 = require('./routes/v2');
 
-
-var app = express();
+const app = express();
 sequelize.sync();
 passportConfig(passport);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8002);
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
   saveUninitialized: false,
@@ -46,25 +40,24 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', router);
+app.use('/v1',v1);
+app.use('/v2',v2);
 app.use('/auth', authRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
+app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
+
 exports.isLoggedIn = (req, res, next) =>{
     if(req.isAuthenticated()){
         next();
@@ -17,4 +20,47 @@ exports.isNotLoggedIn = (req, res, next ) => {
     else{
         res.redirect('/');
     }
+};
+
+
+exports.verifyToken = (req, res, next) => {
+    try{
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    }
+    catch(error){
+        if(error.name === 'TokenExpiredError'){
+            return res.status(419).json({
+                code : 419,
+                messgae : '토큰이 만료',
+            });
+        }
+
+        return res.status(401).json({
+            code : 401,
+            messgae : '유효하지 않는 토큰',
+        });
+    }
+    
+};
+
+
+exports.apiLimiter = new RateLimit({
+    windowMs : 60*1000,
+    max : 9999,
+    delayMs : 0,
+    handler(req, res){
+        res.status(this.statusCode).json({
+            code : this.statusCode,
+            message : '1분에 한 번만 요청할 수 있습니다.',
+        });
+    },
+});
+
+
+exports.deprecated = (req, res) =>{
+    res.status(410).json({
+        code : 410,
+        messgae : '새로운 버전이 나왔습니다.',
+    });
 };
